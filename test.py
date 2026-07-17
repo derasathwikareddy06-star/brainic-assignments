ae39698e02ae2af03260c026a1fd3a9613463b8f
sathwika reddy <derasathwikareddy06@gmail.com>
2026-07-17 14:53:54 +0530

    day7

--- test.py
+++ test.py
@@ -17,4 +17,13 @@ else:
             break
 
     print(prime)
-    
+    balance = int(input("Enter balance: "))
+amount = int(input("Enter withdrawal amount: "))
+
+if amount > balance:
+    print("Insufficient Balance")
+elif amount % 100 != 0:
+    print("Amount should be multiple of 100")
+else:
+    print("Withdrawal Successful")
+    print("Remaining Balance:", balance - amount)
